24b11b81ef1c09864fe0dec8bedf4fe260deea83
Murillo-pedroso <43767213+Murillo-pedroso@users.noreply.github.com>
2020-04-11 21:05:25 -0300

    Add files via upload

---
+++ Aula 4/aula4.php
@@ -0,0 +1,45 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Tomada de decisões e comandos de condição</h1>    
+   
+   <p><h2>Parte 1:</h2>
+   Maria foi ao mercado e sua mae pediu que ela comprasse maçã,</br> 
+   mas se o numero de maçãs disponiveis for menor ou igual a 4 </br>
+   ela deve comprar bananas. 
+   </br></br>
+   
+   <h2>Parte 2:</h2> 
+   Se não houver bananas comprar manga</br>
+   </p>
+
+    <?php
+
+        $maca = 2;
+        $banana = 0;
+        
+        if($maca > 4){
+            echo"comprar maçã";
+
+        }else{
+            if($banana>0){
+                echo"comprar banana";
+
+            }else{
+                
+                echo"Comprar manga";
+            }
+           
+        }
+       
+    ?>
+
+
+</body>
+</html>
---
+++ Aula 4/aula4pt2.php
@@ -0,0 +1,48 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>if/ else/ else if</h1>
+    <p> João quer classificar as turmas de natação de sua academia </br>
+    de acordo com as idades de cada aluno:</br></br>
+
+    Até 4 anos -> Turma A</br>
+    Entre 5 e 8 anos -> Turma B</br>
+    Entre 9 e 13 anos -> Turma C</br>
+    Entre 14 e 17 anos -> Turma D</br>
+    E acima de 18 anos -> Turma E</br></br>
+
+    <?php
+
+        $idade = 3;
+
+            if( $idade <= 4){
+
+                echo"Turma A";
+
+            }else if($idade>4 && $idade<=8){
+
+                echo"Turma B";
+
+            }else if($idade>8 && $idade<=13){
+
+                echo"Turma C";
+
+            }else if($idade>13 && $idade<=17){
+
+                echo"Turma D";
+
+            }else{
+
+                echo"Turma E";
+            }
+
+
+    ?>
+
+</body>
+</html>
